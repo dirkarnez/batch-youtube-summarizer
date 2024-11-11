b5279d736e7355a2b6d3b696f2047dbbdf05d942
dirkarnez <smalldirkalex@gmail.com>
2024-11-11 22:47:42 +0800

    - update run.cmd

--- run.cmd
+++ run.cmd
@@ -2,6 +2,11 @@
 set PYTHON_DIR=%USERPROFILE%\Downloads\python-3.10.8-amd64-portable
 set PATH=%PYTHON_DIR%;%PYTHON_DIR%\Scripts
 
-python main.py --video-id="-6RSGVpS_ik"
+:end
+cls &&^
+set /p id="Enter video id: "
+python main.py --video-id="%id%" &&^
+pause &&^
+goto end
 
 pause
